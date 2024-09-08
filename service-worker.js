a6d99c0595dc99051f3b03213f3b9261890ef5ea
Asma Robbana <nouiri.asma>
2024-09-07 22:47:31 -0400

    manifest.js correction liens image+service worker

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/nos-offres.html',
+  '/contact.html',
+  '/styles.css',
+  '/images/'
+  '/images/pwa/192-maskable-icon.png',
+  '/images/pwa/144-maskable-icon.png',
+  '/images/pwa/maskable-icon.png',
+  '/tailwind.config.js',
+  '/validation.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
